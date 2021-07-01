d9ae913eea1756d2e722c271e3710dc690ef0679
DESKTOP-06IB626\alfre <alfredofweb@gmail.com>
2021-07-01 13:44:51 +0200

    Data pinta en el html y loading incluído

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -9,7 +9,11 @@ import { ClimaService } from 'src/app/services/clima.service';
 export class DashboardComponent implements OnInit {
   urlImagen = 'https://image.flaticon.com/icons/png/512/1116/1116453.png';
   ciudad = "";
-
+  loading = false;
+  temperatura;
+  humedad: 0;
+  clima: "";
+  query = false;
   constructor(private _climaService: ClimaService) { }
 
   ngOnInit(): void {
@@ -17,9 +21,17 @@ export class DashboardComponent implements OnInit {
 
 
   obtenerClima() {
-    console.log(this.ciudad);
+    this.query = false;
+    this.loading = true;
+
     this._climaService.getClima(this.ciudad).subscribe(data => {
+      this.loading = false;
+      this.query = true;
+      this.temperatura = data.main.temp - 273;
+      this.humedad = data.main.humidity;
+      this.clima = data.weather[0].description;
       console.log(data);
-    })
+
+    });
   }
 }
